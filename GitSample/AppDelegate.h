ab50275aca66e757a5f93b2db00f9371d7075612
smkalaivanan <kalaivanan@falconnect.in>
2017-03-31 12:27:00 +0530

    Initial Commit

---
+++ GitSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitSample
+//
+//  Created by apple on 3/31/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitSample
+//
+//  Created by apple on 3/31/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
